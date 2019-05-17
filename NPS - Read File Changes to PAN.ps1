e235074cb6805b2a195b41ca73d8fd4d546fbf42
Nick Shores <nick@shoresmedia.com>
2019-05-17 11:52:13 -0700

    Update NPS - Read File Changes to PAN.ps1

--- NPS - Read File Changes to PAN.ps1
+++ NPS - Read File Changes to PAN.ps1
@@ -1,4 +1,8 @@
-﻿<#
+<#
+##Modified by Nick Shores - 5-17-19
+- Script updated to read new XML format used in newer versions of NPS server
+
+
 How to Setup:
 For your NPS log files (NPS -> Accounting -> Log File Properties):
 Check the boxes for Accounting Requests and Periodic Accounting Status
@@ -10,6 +14,7 @@ Run this script as a scheduled task that starts running at 12:00:05AM daily.
 Call with powershell.exe -NoExit -File "C:\scripts\PANuserID\NPS - Read File Changes to PAN.ps1" (change if needed)
 Set the task to stop existing instances when a new one starts (Under Settings when setting up the task, very bottom).
 Update the CHANGE ME IF NEEDED info below to match log and script locations as well as your PAN URL and API key.
+
 
 #>
 
@@ -32,33 +37,32 @@ $fsw = New-Object IO.FileSystemWatcher $folder, $filter -Property @{IncludeSubdi
 
 #register file system watcher object as an event, start monitoring file
 Register-ObjectEvent $fsw Changed -SourceIdentifier PAN-UserID-Mapping -Action {
+#Bypass TLS check
+[System.Net.ServicePointManager]::ServerCertificateValidationCallback = {$true}
 $path = $Event.SourceEventArgs.FullPath
+Out-File -FilePath C:\scripts\PANuserID\debug.txt -Append -InputObject "Path is $path"
 
-#get last line of NPS log
-$newLine = (Get-Content -Tail 1 $path) -split ','
+#get last line of NPS log, create an XML object
+[xml] $xdoc = (Get-Content -Tail 1 $path)
 
 #assign variable for user, NPS has username in 6th column (index 5)
-$newUser = $($newline[5] -replace '"','').Split('@')[0]
-if ($newUser.Contains('\')){
-    $newUser = $newUser.Split('\')[1]
-}
+$newUser = $xdoc.Event.'User-Name'.'#text'
 
-#get IP info from column 11 (index 10)
-$newIP = $newline[10] -replace '"',''
+#grab ip of user
+$newIP = $xdoc.Event.'NAS-IP-Address'.'#text'
 #create final user variable with PAN formated username
-# CHANGE ME - domain
-$user = "<domain>\" + $newUser.ToLower()
+$user = $newUser.ToLower()
 
 # Optional log file that can be created to monitor what's being pulled from the log file.
-#Out-File -FilePath C:\scripts\PANuserID\config_changes.txt -Append -InputObject "$(Get-Date), Username: $user, IP Address: $newIP"
+Out-File -FilePath C:\scripts\PANuserID\config_changes.txt -Append -InputObject "$(Get-Date), Username: $user, IP Address: $newIP"
 
 #dir to find the XML template file
 # CHANGE ME IF NEEDED
-$dir = "C:\scripts\PANuserID"
+$dir = "C:\scripts\"
 
 #full path of the XML template file
 # CHANGE ME IF NEEDED
-$pathXml = $dir + "\userIDfile.xml"
+$pathXml = $dir + "userIDfile.xml"
 
 #read the XML file
 $xml = [xml](Get-Content $pathXml)
@@ -76,7 +80,7 @@ $xml.Save($pathXml)
 #create a web-clietn object, upload XML template file to PAN box
 $wc = New-Object System.Net.WebClient
 # CHANGE ME - PAN URL and API key
-$wc.UploadFile( 'https://<PAN_URL>/api/?type=user-id&key=<PAN_API_KEY>',$pathXml)
+$wc.UploadFile( 'https://10.1.2.243/api/?type=user-id&key=LUFRPT1VYURyVnVtKzk2dCtWajRoVngxTGdLa3ZyRmM9TVlMVHh6VElvR1lQOXE5d25PL3BUZVlON3lvRTdRQWZ4TFRQYkxpNUlOUVlHYzNBaWxhaS95YW5CMnRrS2Z5UTNjYTJ3TFVDQmJHQWpMMUpDeVRZWlE9PQ==',$pathXml)
 
 }
 
